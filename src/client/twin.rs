4af809977683268ee044931bf75f1d8ffc3434f2
Buell <50990105+JanZachmann@users.noreply.github.com>
2023-05-02 08:53:21 +0200

    added Copy and Clone trait to enums for better usability in library clients (#30)

--- src/client/twin.rs
+++ src/client/twin.rs
@@ -15,7 +15,7 @@ pub struct DeviceTwin {
 }
 
 /// type of client twin
-#[derive(Debug, PartialEq)]
+#[derive(Copy, Clone, Debug, PartialEq)]
 pub enum ClientType {
     /// edge module twin client
     Edge,
